864df92a82089992ac74a02679e62ce17672619c
Elio Tovele <eliotoni@outlook.com>
2023-07-04 21:33:18 +0200

    optimizing images

--- src/services/data.ts
+++ src/services/data.ts
@@ -3,7 +3,7 @@ export const products = [
     id: 1,
     title: 'Mochila para laptops',
     price: 179.9,
-    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    image: 'https://res.cloudinary.com/eliotovele/image/upload/v1688498582/81fPKd-2AYL._AC_SL1500__dyc1no.jpg',
     description: 'Seu pacote perfeito para o uso diário e passeios na floresta. Guarde o seu portátil (até 15 polegadas) na manga acolchoada, o seu dia-a-dia"'
   },
   {
@@ -11,35 +11,35 @@ export const products = [
     title: 'Camisetas Masculinas Casuais',
     price: 139.9,
     image:
-      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      'https://res.cloudinary.com/eliotovele/image/upload/v1688498735/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY__umjhkj.jpg',
     description: 'Estilo fino, manga comprida raglan contrastante, placket henley de três botões, tecido leve e macio para um uso respirável e confortável. E camisas sólidas costuradas com pescoço redondo feitas para durabilidade e um ótimo ajuste para roupas casuais e fãs de basebol obstinados. O decote redondo estilo Henley inclui um placket de três botões.'
   },
   {
     id: 3,
     title: 'Jaqueta moderna de Algodão Masculina',
     price: 219.9,
-    image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    image: 'https://res.cloudinary.com/eliotovele/image/upload/v1688499002/71li-ujtlUL._AC_UX679__j6wthq.jpg',
     description: 'Jaqueta para a primavera / outono / inverno, adequado para muitas ocasiões, como trabalho, caminhadas, camping, montanha / escalada, ciclismo, viagens ou outros ao ar livre. Boa escolha de presente para você ou seu familiar. Um amor caloroso ao Pai, marido ou filho neste dia de ação de graças ou de Natal.'
   },
   {
     id: 5,
     title: 'Roupa Casual Masculina Slim Fit',
     price: 139.9,
-    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    image: 'https://res.cloudinary.com/eliotovele/image/upload/v1688499064/71YXzeOuslL._AC_UY879__p2hefu.jpg',
     description: 'A cor pode ser ligeiramente diferente entre na tela e na prática. Por favor, note que as construções do corpo variam de pessoa para pessoa, portanto, informações detalhadas de tamanho devem ser revisadas abaixo na descrição do produto.'
   },
   {
     id: 6,
     title: 'Pulseira de Corrente John Hardy',
     price: 219.9,
-    image: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+    image: 'https://res.cloudinary.com/eliotovele/image/upload/v1688499127/71pWzhdJNwL._AC_UL640_QL65_ML3__ged2d2.jpg',
     description: 'Da nossa Coleção Legends, o Naga foi inspirado no mítico dragão de água que protege a pérola do oceano. Use de frente para dentro para ser agraciado com amor e abundância, ou para fora para proteção.'
   },
   {
     id: 4,
     title: 'Anel Solid Gold Petite Micropave',
     price: 179.9,
-    image: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg',
+    image: 'https://res.cloudinary.com/eliotovele/image/upload/v1688499081/61sbMiUnoGL._AC_UL640_QL65_ML3__eoeeuw.jpg',
     description: 'Satisfação Garantida. Devolver ou trocar qualquer encomenda no prazo de 30 dias. Projetado e vendido pelo Hafeez Center nos Estados Unidos. Satisfação Garantida. Devolver ou trocar qualquer encomenda no prazo de 30 dias.'
   }
 ];
